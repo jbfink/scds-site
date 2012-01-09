69fa00d83bd2c4bd3316c31be9bf0d81a72ec72f
jbfink <john.fink@gmail.com>
2012-01-09 12:56:50 -0500

    lx5 specific changes.

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/scds/"
+http_images_path = "/scds/images"
+http_fonts_path = "/scds/fonts"
+css_dir = "public/scds/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
